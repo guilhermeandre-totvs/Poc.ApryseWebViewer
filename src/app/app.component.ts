8e0d4d893cf3a0b4811d64e04ec47f4c477a9044
Guilherme Andre de Melo <guilhermeanm95@hotmail.com>
2024-02-21 11:27:41 -0300

    setting application on app root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import WebViewer, { WebViewerInstance } from "@pdftron/webviewer";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  styleUrls: ["app.component.css"],
+  templateUrl: "app.component.html",
 })
-export class AppComponent {
-  title = 'poc-apryse';
+export class AppComponent implements AfterViewInit {
+  wvInstance?: WebViewerInstance;
+
+  @ViewChild("viewer") viewer!: ElementRef;
+
+  @Output() coreControlsEvent: EventEmitter<string> = new EventEmitter();
+
+  private documentLoaded$: Subject<void>;
+
+  constructor() {
+    this.documentLoaded$ = new Subject<void>();
+  }
+
+  ngAfterViewInit(): void {
+    WebViewer(
+      {
+        path: "../lib",
+        licenseKey:
+          "demo:1708437239140:7f5b844a030000000012a8cd1213f051d99456c64dd2f23c9c80561d12", // sign up to get a free trial key at https://dev.apryse.com
+      },
+      this.viewer.nativeElement)
+  }
 }
