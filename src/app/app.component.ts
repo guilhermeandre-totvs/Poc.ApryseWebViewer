cbd094d55ca008ba0180ab83695ca8ae7e813cf0
Guilherme Andre de Melo <guilhermeanm95@hotmail.com>
2024-02-26 12:24:21 -0300

    complementando o arquivo ts com a logica de upload de arquivos e travas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,7 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  EventEmitter,
-  OnInit,
-  Output,
-  ViewChild,
-} from "@angular/core";
-import WebViewer, { WebViewerInstance } from "@pdftron/webviewer";
-import { Subject, of } from "rxjs";
-
+import {AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild, ChangeDetectorRef } from '@angular/core';
+import WebViewer, {WebViewerInstance} from "@pdftron/webviewer";
+import {Subject} from "rxjs";
 import { ApryseService } from './agreement.service';
-
 
 @Component({
   selector: "app-root",
@@ -21,16 +11,24 @@ import { ApryseService } from './agreement.service';
 export class AppComponent implements AfterViewInit {
   wvInstance?: WebViewerInstance;
 
+  page: number = 1
+  currentPage: number | undefined
+  valid: boolean = true
+  check: number = 1;
+  totalPages: number = 1;
+
+  teste: number = 1
+
   @ViewChild("viewer") viewer!: ElementRef;
   @ViewChild("upload") upload!: ElementRef;
-
 
   @Output() coreControlsEvent: EventEmitter<string> = new EventEmitter();
 
   private documentLoaded$: Subject<void>;
 
   constructor(
-    public apryseService: ApryseService
+    public apryseService: ApryseService,
+    private changeDetectorRef: ChangeDetectorRef
   ) {
     this.documentLoaded$ = new Subject<void>();
   }
@@ -42,7 +40,8 @@ export class AppComponent implements AfterViewInit {
         licenseKey:
           "1708437239140:7f5b844a030000000012a8cd1213f051d99456c64dd2f23c9c80561d12", // sign up to get a free trial key at https://dev.apryse.com
       },this.viewer.nativeElement) .then(instance => {
-        // Sets the current toolbar group
+        const { documentViewer } = instance.Core;    
+        
         instance.UI.disableElements(['toolbarGroup-Shapes']);
         instance.UI.disableElements(['toolbarGroup-Edit']);
         instance.UI.disableElements(['toolbarGroup-Insert']);
@@ -68,13 +67,25 @@ export class AppComponent implements AfterViewInit {
               for (var i = 0; i < byteString.length; i++) {
                 uintArray[i] = byteString.charCodeAt(i);
               }
-
+    
               const file = new File([arrayByffer], 'teste', { type: 'application/pdf' });
               instance.UI.loadDocument(file)
-              console.log(v.fileBytes)
             }
           })
         }
+    
+        documentViewer.addEventListener('documentLoaded', () => {
+          const actualPage = documentViewer.getPageCount()
+          this.valid = actualPage === 1 ? false : true
+          this.changeDetectorRef.detectChanges();
+          
+        })
+    
+        documentViewer.addEventListener('pageNumberUpdated', pageNumber => {
+          const actualPage = documentViewer.getPageCount();
+          this.valid = actualPage === pageNumber ? false : true
+          this.changeDetectorRef.detectChanges();     
+        });
       })
   }
 }
